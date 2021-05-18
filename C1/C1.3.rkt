96c6ff7f77c7dd1ba9f0839f854d80ffcd5a30fc
Raj <rajp152000@gmail.com>
2021-05-18 09:39:56 +0530

    1.40-1.46

--- C1/C1.3.rkt
+++ C1/C1.3.rkt
@@ -35,6 +35,7 @@
   (product identity 1 1 n))
 
 (define (square x) (* x x))
+(define (cube x) (* x x x))
 
 (define (pi  n)
   (* (/ (* 2
@@ -84,7 +85,7 @@
 
 ;1.35
 
-(define (fixed-point-find fn fg thresh)
+(define (fixed-point-find fn fg [thresh 0.0001])
   "find the fixed point of fn(x) with first guess as fg"
   (define (close-enough? a b)
     (< (abs (- a
@@ -180,3 +181,125 @@
   (let ((n (lambda (k) (expt x k)))
         (d (lambda (k) (- (* 2 k) 1))))
     (cont-frac-iter-neg n d k)))
+
+
+; prereqs
+
+(define dx 0.00001)
+(define (deriv f)
+  (lambda (x)
+    (/ (- (f (+ x dx))
+          (f x))
+       dx)))
+
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point-find (newton-transform g) guess))
+
+;1.40
+
+(define (cubic a b c)
+  (lambda (x)
+    (+ (* (cube x))
+       (* a (square x))
+       (* b x)
+       c)))
+
+;1.41
+
+(define (double [f identity])
+  (lambda (x) (f (f x))))
+
+;1.42
+
+(define (compose f g)
+  (lambda (x)
+    (f (g x))))
+
+;1.43
+
+(define (repeated f n)
+  (define (repeated-cache curr res-func)
+    (if (= curr n)
+        res-func
+        (repeated-cache (+ curr 1)
+                        (compose f res-func))))
+  (repeated-cache 0 identity))
+
+;1.44
+
+(define (smooth f)
+  (lambda (x)
+    (/ (+ (f x)
+          (f (+ x dx))
+          (f (- x dx)))
+       3)))
+
+(define (n-smooth f n)
+  (lambda (x)
+    (((repeated smooth n) f) x)))
+
+
+;prereqs
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) 0.0001))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (average-damp f)
+  (lambda (x)
+    (/ (+ x (f x))
+       2)))
+
+(define (fixed-point-of-transform g transform guess)
+  (fixed-point (transform g) guess))
+
+;1.45
+
+(define (nth-root x n)
+  (let ((transform (repeated average-damp (quotient n 2)))
+        (g (lambda (y) (/ x (expt y (- n 1))))))
+    (fixed-point-of-transform g transform 1.0)))
+
+;(nth-root 1024 10)
+;>2.0001827764147846
+
+;1.46
+
+(define (iterative-improve good? improve)
+  (define (guess-iter guess)
+    (if (good? guess)
+        guess
+        (guess-iter (improve guess))))
+  guess-iter)
+
+(define (sqrt x)
+  (define (good? guess)
+    (< (abs (- (square guess) x))
+       0.0001))
+  (define (improve guess)
+    (/ (+ (/ x guess)
+          guess)
+       2))
+  ((iterative-improve good? improve) 1.0))
+
+(define (gen-fixed-point f guess)
+  (define (close-enough? a b)
+    (< (abs (- a b)) 0.00001))
+  (define (good? guess)
+    (close-enough? guess (f guess)))
+  (define (improve guess)
+    (/ (+ guess
+          (f guess))
+       2))
+  ((iterative-improve good? improve) guess))
